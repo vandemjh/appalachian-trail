77399b007fb153090ae948f4c977695b690ddeda
vandemjh <jack@jackv.dev>
2021-02-17 22:58:31 -0500

    removed start link

--- social-bot/index.ts
+++ social-bot/index.ts
@@ -8,6 +8,5 @@ const port = process.env.PORT ?? 80;
 useRouter(app);
 
 app.listen(port, () => {
-  console.log(`Server started at http://localhost:${port}
-  Sign in at http://localhost:${port}/start`);
+  console.log(`Server started at http://localhost:${port}`);
 });
