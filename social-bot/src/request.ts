d188509c6d823b04ce871dccb98a824b9c8656f3
vandemjh <jack@jackv.dev>
2021-02-17 22:58:22 -0500

    Parted out function for hitting api

---
+++ social-bot/src/request.ts
@@ -0,0 +1,37 @@
+import https from 'https';
+require('dotenv').config();
+
+export default async function getPhotos(
+  token: string,
+  field: 'albums' | 'mediaItems' | 'mediaItems:search',
+  id?: string,
+  body?: object,
+): Promise<object> {
+  const options = {
+    method: 'GET',
+    host: `photoslibrary.googleapis.com`,
+    path: `/v1/${field + (id ? '/' + id : '')}?key=${process.env.API_KEY}`,
+    headers: {
+      Authorization: `Bearer ${token}`,
+      Accept: 'application/json',
+    },
+    body: body,
+  };
+
+  return new Promise((resolve, reject) => {
+    var data: string = '';
+    https
+      .request(options, (resp) => {
+        resp.on('data', (chunk) => {
+          data += chunk;
+        });
+        resp.on('end', () => {
+          resolve(JSON.parse(data));
+        });
+        resp.on('error', (err) => {
+          reject(err);
+        });
+      })
+      .end();
+  });
+}
