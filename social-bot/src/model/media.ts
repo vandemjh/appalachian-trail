a2dd4d00d13fda6b0cf691aef2907351f81eb6a5
vandemjh <jack@jackv.dev>
2021-02-21 19:16:16 -0500

    Added picture class, moved interface to media

---
+++ social-bot/src/model/media.ts
@@ -0,0 +1,8 @@
+interface Media {
+  id: string;
+  productUrl: string;
+  baseUrl: string;
+  mimeType: string;
+  mediaMetadata: Metadata;
+  filename: object;
+}
--- social-bot/src/model/picture.ts
+++ social-bot/src/model/picture.ts
@@ -1,8 +1,22 @@
-interface Picture {
-    id: string;
-    productUrl: string;
-    baseUrl: string;
-    mimeType: string;
-    mediaMetadata: Metadata;
-    filename: object;
+export class Picture implements Media {
+  id: string;
+  productUrl: string;
+  baseUrl: string;
+  mimeType: string;
+  mediaMetadata: Metadata;
+  filename: object;
+  fullSizeUrl: string;
+  downloadUrl: string;
+  constructor(obj: any) {
+    this.id = obj.id;
+    this.productUrl = obj.productUrl;
+    this.baseUrl = obj.baseUrl;
+    this.mimeType = obj.mimeType;
+    this.mediaMetadata = obj.mediaMetadata;
+    this.filename = obj.filename;
+    this.fullSizeUrl =
+      this.baseUrl +
+      `=w${this.mediaMetadata.width}-h${this.mediaMetadata.height}`;
+    this.downloadUrl = this.baseUrl + `=d`;
   }
+}
